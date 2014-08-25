a96f8f821cf33b0b5c3df43bf3174f058dbc7546
Gabor Csardi <csardi.gabor@gmail.com>
2014-08-25 00:49:23 -0400

    Refine OSX script, remove gcc specific configure options

--- r-build-osx.sh
+++ r-build-osx.sh
@@ -33,20 +33,15 @@ Configure() {
 
     R_PAPERSIZE=letter                              \
     R_BATCHSAVE="--no-save --no-restore"            \
-    R_BROWSER=xdg-open                              \
-    PAGER=/usr/bin/pager                            \
     PERL=/usr/bin/perl                              \
     R_UNZIPCMD=/usr/bin/unzip                       \
     R_ZIPCMD=/usr/bin/zip                           \
     R_PRINTCMD=/usr/bin/lpr                         \
-    LIBnn=lib                                       \
     AWK=/usr/bin/awk                                \
-    CC="ccache gcc"                                 \
-    CFLAGS="-ggdb -pipe -std=gnu99 -Wall -pedantic" \
-    CXX="ccache g++"                                \
-    CXXFLAGS="-ggdb -pipe -Wall -pedantic"          \
-    FC="ccache gfortran"                            \
-    F77="ccache gfortran"                           \
+    CFLAGS="-std=gnu99 -Wall -pedantic"             \
+    CXXFLAGS="-Wall -pedantic"                      \
+    FC="gfortran"                                   \
+    F77="gfortran"                                  \
     MAKE="make"                                     \
     ./configure                                     \
     --prefix=$HOME/R/R-devel                        \
