e092aec163407e9bbecc8af11e13b0a63e3e62d9
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-16 11:07:00 -0400

    Fix creating build script

--- get-build-script.sh
+++ get-build-script.sh
@@ -2,5 +2,5 @@
 
 git remote set-url origin https://github.com/gaborcsardi/r-builder
 git fetch origin master
-git show origin/master:r-build.sh > r-build.sh
+git show master:r-build.sh > r-build.sh
 chmod +x r-build.sh
