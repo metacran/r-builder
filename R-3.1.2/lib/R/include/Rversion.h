0afe8a2ca77813db23a10cd91dc8c24bd9a743b1
Gabor Csardi <csardi.gabor@gmail.com>
2014-11-04 03:24:24 +0000

    Building R 3.1.2 on semaphore

---
+++ R-3.1.2/lib/R/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 196866
+#define R_NICK "Pumpkin Helmet"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "1.2"
+#define R_STATUS ""
+#define R_YEAR   "2014"
+#define R_MONTH  "10"
+#define R_DAY    "31"
+#define R_SVN_REVISION 66913
+#define R_FILEVERSION    3,12,66913,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
