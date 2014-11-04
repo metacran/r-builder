11c0b3586b7afb2de39bbc490305037e14e82253
Gabor Csardi <csardi.gabor@gmail.com>
2014-11-04 11:27:36 -0500

    gaborcsardi -> metacran, #12
    [ci skip]

--- get-build-script.sh
+++ get-build-script.sh
@@ -1,4 +1,4 @@
 #! /bin/bash -ex
 
-curl -O https://raw.githubusercontent.com/gaborcsardi/r-builder/master/r-build.sh
+curl -O https://raw.githubusercontent.com/metacran/r-builder/master/r-build.sh
 chmod +x r-build.sh
