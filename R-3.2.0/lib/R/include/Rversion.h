972972dff8e92dab3f0d231f343e24cb75ad0acf
Gabor Csardi <csardi.gabor@gmail.com>
2015-04-22 15:20:53 +0000

    Building R 3.2.0 on travis

---
+++ R-3.2.0/lib/R/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 197120
+#define R_NICK "Full of Ingredients"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "2.0"
+#define R_STATUS ""
+#define R_YEAR   "2015"
+#define R_MONTH  "04"
+#define R_DAY    "16"
+#define R_SVN_REVISION 68180
+#define R_FILEVERSION    3,20,68180,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
