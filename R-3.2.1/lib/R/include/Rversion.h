1a961c396fbc019c9f1d538bba3ed69109de006a
Gabor Csardi <csardi.gabor@gmail.com>
2015-06-22 06:33:32 +0000

    Building R 3.2.1 on semaphore

---
+++ R-3.2.1/lib/R/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 197121
+#define R_NICK "World-Famous Astronaut"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "2.1"
+#define R_STATUS ""
+#define R_YEAR   "2015"
+#define R_MONTH  "06"
+#define R_DAY    "18"
+#define R_SVN_REVISION 68531
+#define R_FILEVERSION    3,21,68531,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
