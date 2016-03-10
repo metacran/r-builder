3e8862e36e0841ffde2cb200d3121e700ac21e42
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-10 13:10:18 +0000

    Building R 3.2.4 on semaphore

---
+++ R-3.2.4/lib/R/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 197124
+#define R_NICK "Very Secure Dishes"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "2.4"
+#define R_STATUS ""
+#define R_YEAR   "2016"
+#define R_MONTH  "03"
+#define R_DAY    "10"
+#define R_SVN_REVISION 70301
+#define R_FILEVERSION    3,24,70301,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
