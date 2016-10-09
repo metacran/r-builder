9d5f2743b11310f60eb590e3ff34515e434f682d
Gabor Csardi <csardi.gabor@gmail.com>
2016-10-09 16:13:48 +0000

    Building R devel on semaphore

---
+++ R-devel/lib/R/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 197632
+#define R_NICK "Unsuffered Consequences"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "4.0"
+#define R_STATUS "Under development (unstable)"
+#define R_YEAR   "2016"
+#define R_MONTH  "10"
+#define R_DAY    "09"
+#define R_SVN_REVISION 71477
+#define R_FILEVERSION    3,40,71477,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
