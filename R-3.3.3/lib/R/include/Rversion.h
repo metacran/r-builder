0ec25ae93c4f92600369a6b921068897e055a3ec
Gabor Csardi <csardi.gabor@gmail.com>
2017-03-12 10:10:10 +0000

    Building R 3.3.3 on travis

---
+++ R-3.3.3/lib/R/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 197379
+#define R_NICK "Another Canoe"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "3.3"
+#define R_STATUS ""
+#define R_YEAR   "2017"
+#define R_MONTH  "03"
+#define R_DAY    "06"
+#define R_SVN_REVISION 72310
+#define R_FILEVERSION    3,33,72310,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
