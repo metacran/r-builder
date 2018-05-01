2b4258c4e8061c6b245fb1eaa507e2ed85a44230
Gabor Csardi <csardi.gabor@gmail.com>
2018-05-01 17:45:41 +0000

    Building R devel on travis

---
+++ R-devel/lib/R/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 198144
+#define R_NICK "Unsuffered Consequences"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "6.0"
+#define R_STATUS "Under development (unstable)"
+#define R_YEAR   "2018"
+#define R_MONTH  "05"
+#define R_DAY    "01"
+#define R_SVN_REVISION 74677
+#define R_FILEVERSION    3,60,74677,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
